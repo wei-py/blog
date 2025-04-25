5ebcbaecb475d7f76aa74a88d4458bade07830f8
wei-py <rabbwei@outlook.com>
2025-04-26 03:56:30 +0800

    feat: 添加 Cloudflare 环境支持并调整基础路径
    在 package.json 中添加了 `cloudflare` 脚本，用于在 Cloudflare 环境下构建项目。同时，在 .vitepress/config.ts 中根据 `NODE_ENV` 动态调整 `base` 路径，确保在 Cloudflare 环境下使用根路径 `/`，而在其他环境下使用 `/blog/`。此外，导航链接也相应调整为根路径。

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,11 +5,12 @@ import { getPosts } from './theme/serverUtils'
 const pageSize = 10
 
 // const isProd = process.env.NODE_ENV === 'production'
+const isCloudflare = process.env.NODE_ENV === 'cloudflare'
 const isProd = true
 
 export default defineConfig({
     title: 'wei',
-    base: '/blog/',
+    base: isCloudflare ? '/' : '/blog/',
     cacheDir: './node_modules/vitepress_cache',
     description: 'vitepress,blog,blog-theme',
     ignoreDeadLinks: true,
@@ -23,31 +24,31 @@ export default defineConfig({
             issueTerm: 'pathname'
         },
         nav: [
-            { text: '首页', link: '/blog' },
-            { text: '分类', link: '/blog/pages/category' },
-            { text: '归档', link: '/blog/pages/archives' },
-            { text: '标签', link: '/blog/pages/tags' },
+            { text: '首页', link: '/' },
+            { text: '分类', link: '/pages/category' },
+            { text: '归档', link: '/pages/archives' },
+            { text: '标签', link: '/pages/tags' }
             // { text: 'About', link: '/pages/about' }
             // { text: 'Airene', link: 'http://airene.net' }  -- External link test
         ],
         search: {
-            provider: 'local',
+            provider: 'local'
         },
         //outline:[2,3],
-        outline:{
-            label:'文章摘要'
-        },
+        outline: {
+            label: '文章摘要'
+        }
         // socialLinks: [{ icon: 'github', link: 'https://github.com/airene/vitepress-blog-pure' }]
     } as any,
 
     srcExclude: isProd
-    ? [
-          '**/trash/**/*.md', // 排除所有 trash 目录
-          '**/draft/**/*.md', // 递归排除子目录
-          '**/private-notes/*.md', // 排除特定文件
-          'README.md'
-      ]
-    : ['README.md'],
+        ? [
+              '**/trash/**/*.md', // 排除所有 trash 目录
+              '**/draft/**/*.md', // 递归排除子目录
+              '**/private-notes/*.md', // 排除特定文件
+              'README.md'
+          ]
+        : ['README.md'],
     vite: {
         //build: { minify: false }
         server: { port: 5000 }
