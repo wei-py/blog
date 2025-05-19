50c356885c90601defa5666d6979bdfce8a0ae9d
wei-py <rabbwei@outlook.com>
2025-05-20 01:16:08 +0800

    chore(vscode): 更新 VSCode 设置和 ESLint 配置
    - 在 VSCode 设置中启用格式化保存功能
    - 修改 ESLint 配置，启用所有格式化规则并调整 Vue 设置

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
 import antfu from "@antfu/eslint-config";
 
 export default antfu({
-  formatters: {
-    "style/no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
-  },
+  // formatters: {
+  //   "style/no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
+  // },
+  formatters: true,
   vue: true,
   stylistic: {
     semi: true,
     indent: 2,
     quotes: "double",
   },
-  // markdown: t,
 });
