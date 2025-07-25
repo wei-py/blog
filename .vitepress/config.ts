ca579ce89083521f6c3c3786376847cb5da27e4f
wei-py <rabbwei@outlook.com>
2025-07-25 17:18:38 +0800

    refactor(vitepress): 优化页面布局和样式
    - 调整内容区域样式，优化字体大小和排版
    - 修复侧边栏显示问题，调整位置和层级
    - 优化文章列表布局，居中对齐
    -

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,5 @@
 import AutoNav from "vite-plugin-vitepress-auto-nav";
+import VitePressPluginAutoNavSidebar from '@movk-repo/vitepress-plugin-auto-nav-sidebar'
 import { defineConfig } from "vitepress";
 import withMindMap from '@dhlx/vitepress-plugin-mindmap'
 
@@ -50,20 +51,25 @@ export default withMindMap(defineConfig({
 
   srcExclude: isProd
     ? [
-        "**/trash/**/*.md", // 排除所有 trash 目录
-        "**/draft/**/*.md", // 递归排除子目录
-        "**/private-notes/*.md", // 排除特定文件
-        "README.md",
-      ]
+      "**/trash/**/*.md", // 排除所有 trash 目录
+      "**/draft/**/*.md", // 递归排除子目录
+      "**/private-notes/*.md", // 排除特定文件
+      "README.md",
+    ]
     : ["README.md"],
   vite: {
     // build: { minify: false }
     server: { port: 5000 },
-    plugins: [AutoNav({
-      itemsSetting: {
-        ...(await getFolder()),
-      },
-    }) as any],
+    plugins: [
+      AutoNav({
+        itemsSetting: {
+          ...(await getFolder()),
+        },
+      }) as any,
+      // VitePressPluginAutoNavSidebar({
+      //   documentRootPath: "/posts/",
+      // }) as any,
+    ],
   },
   /*
       optimizeDeps: {
